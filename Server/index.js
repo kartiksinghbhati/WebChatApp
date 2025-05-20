08fdfa3d2a12a24365ffe7d8ab9e2caea0e86348
Kartik Singh Bhati <95916448+kartiksinghbhati@users.noreply.github.com>
2025-05-20 22:00:45 +0530

    url update in socket io

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,7 @@ const server = app.listen(process.env.PORT, ()=>{
 
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://web-chat-app-kartiksinghbhati.vercel.app",
       credentials: true,
     },
   });
